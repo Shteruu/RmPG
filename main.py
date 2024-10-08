237d03c36a2158bae5b9163f62d0dd8291280f64
Shiteru <Kuskos4work@gmail.com>
2024-10-08 21:37:52 +0300

    v0.0 the beginning of development
    This reverts commit 4880b7f7

--- main.py
+++ main.py
@@ -1,8 +1,67 @@
 import arcade
-WIDTH = 100
-HEIGHT = 100
+
+WIDTH = 800
+HEIGHT = 600
+
+
+class Entity:
+    def __init__(self, posX=0, posY=0, width=1, height=1, color=arcade.color.RED):
+        self.x = posX
+        self.y = posY
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self):
+        arcade.draw_rectangle_filled(
+            self.x,
+            self.y,
+            self.width,
+            self.height,
+            self.color
+        )
+
+
+class Player(Entity):
+
+    def __init__(self, posX, posY, width, height, color=arcade.color.RED):
+        super().__init__(posX, posY, width, height, color)
+        self.moveX = 0
+        self.moveY = 0
+
+
+class Game(arcade.Window):
+
+    def __init__(self, width, height, name):
+        super().__init__(width, height, name)
+        arcade.set_background_color(arcade.color.BLUE)
+
+        self.person = Player(100, 100, 50, 100)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.person.draw()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.W:
+            self.person.y += 100
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.W:
+            self.person.y -= 100
 
 
 if __name__ == '__main__':
-    arcade.open_window(WIDTH, HEIGHT)
+    window = Game(WIDTH, HEIGHT, 'RmPG')
+
+    arcade.run()
+
+'''
+to-do list
+
+classes: entity - person - enemy, room - map
+connect sprites
+
+'''
 
