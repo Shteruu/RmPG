81b730e98b8d08e8a985c7a9a138f32140df38e8
Shiteru <Kuskos4work@gmail.com>
2024-10-24 18:29:27 +0300

    v0.3 merge

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import math
 
 PATH = ''#A:\ProjectGame\RmPG\\'
 
-WIDTH = 800
-HEIGHT = 600
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCALING = 1
 
 PLAYER_MOVEMENT_SPEED = 3
 
@@ -12,6 +13,8 @@ RIGHT_FACING = DOWN_RIGHT_FACING = 0
 LEFT_FACING = DOWN_LEFT_FACING = 1
 UP_LEFT_FACING = UP_FACING = UP_RIGHT_FACING = 2
 DOWN_FACING = None
+
+MAP_SIZE = 1000
 
 
 def load_texture_pair(filename, x=0, y=0):
@@ -53,6 +56,11 @@ class Entity(arcade.Sprite):
         self.facing = RIGHT_FACING
         self.cur_texture = 0
 
+
+class Player(Entity):
+
+    def __init__(self):
+        super().__init__()
         self.stay_texture_pair = load_crop_texture_pair(f"{PATH}sprites\\Forward.png", 64, 64)
         self.stay_texture_pair.append(arcade.load_texture(f"{PATH}sprites\\Back.png"))
 
@@ -76,14 +84,8 @@ class Entity(arcade.Sprite):
         self.cur_texture += 1
         if self.cur_texture > 27:
             self.cur_texture = 0
-        
+
         self.texture = self.walk_textures[self.cur_texture // 7][self.facing]
-
-
-class Player(Entity):
-
-    def __init__(self):
-        super().__init__()
 
 
 class Game(arcade.Window):
@@ -100,12 +102,14 @@ class Game(arcade.Window):
         self.person = None
 
         self.map = None
-        
+
         self.mouse_angle = 0
         self.mouse_x = 0
-        self.mouse_y = 0 
+        self.mouse_y = 0
 
-    
+        self.camera = None
+
+
     def on_mouse(self):
         self.mouse_angle = math.atan2(self.mouse_y- self.person.center_y, self.mouse_x - self.person.center_x) * (180 / math.pi)
         if self.mouse_angle > 135 or self.mouse_angle < -90:
@@ -114,26 +118,43 @@ class Game(arcade.Window):
             self.person.facing = RIGHT_FACING
         elif 45 <= self.mouse_angle <= 135:
             self.person.facing = UP_FACING
-            
+
         self.person.update_animation(1/60)
-    
+
     def setup(self):
         self.scene = arcade.Scene()
+
+        self.camera = arcade.Camera(self.width, self.height)
+        self.camera.viewport_width = SCREEN_WIDTH * SCALING
+        self.camera.viewport_height = SCREEN_HEIGHT * SCALING
 
         self.scene.add_sprite_list("Player")
         self.scene.add_sprite_list("Map")
         self.background_texture = arcade.load_texture(f"{PATH}sprites\\background.jpg")
 
         self.person = Player()
-        self.person.center_x = 500
-        self.person.center_y = 400
+        # self.person.scale = 0.5
+        self.person.center_x = SCREEN_WIDTH // 2
+        self.person.center_y = SCREEN_HEIGHT // 2
+        self.person.center = self.person.center_x, self.person.center_y
+
         self.scene.add_sprite("Player", self.person)
         self.scene.add_sprite_list(f"{PATH}sprites\\Forward.png")
 
+    def camera_on_player(self):  # coordinates links to left-down corner
+        screen_center_x = self.person.center_x - SCREEN_WIDTH // 2
+        screen_center_y = self.person.center_y - SCREEN_HEIGHT // 2
+        camera_x = max(min(screen_center_x, MAP_SIZE - SCREEN_WIDTH), 0)
+        camera_y = max(min(screen_center_y, MAP_SIZE - SCREEN_HEIGHT), 0)
+
+        self.camera.move_to((camera_x, camera_y))
+
     def on_draw(self):
         arcade.start_render()
-        
-        arcade.draw_texture_rectangle(WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT, self.background_texture)
+
+        self.camera.use()
+
+        arcade.draw_texture_rectangle(MAP_SIZE // 2, MAP_SIZE // 2, MAP_SIZE, MAP_SIZE, self.background_texture)
 
         self.scene.draw()
 
@@ -143,11 +164,13 @@ class Game(arcade.Window):
         self.on_mouse()
         self.person.update_animation(delta_time)
 
+        self.camera_on_player()
+
     def on_mouse_motion(self, x, y, dx, dy):
         self.mouse_x = x
         self.mouse_y = y
 
-        
+
 
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.A:
@@ -171,7 +194,7 @@ class Game(arcade.Window):
 
 
 def main():
-    window = Game(WIDTH, HEIGHT, 'RmPG')
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, 'RmPG')
     window.setup()
     arcade.run()
 
@@ -182,9 +205,8 @@ if __name__ == '__main__':
 '''
 to-do list
 
-classes: sprite -> entity -> person | enemy, room - map, camera
-
-загружать текстуры игрока в __init__ ИГРОКА!!!
+classes: sprite -> entity -> enemy
+         room - map
 
 Map generator
 Player shooting
