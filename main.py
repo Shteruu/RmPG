694d0838d4ba93368748cef7fab81d8e31061e81
Shiteru <Kuskos4work@gmail.com>
2024-11-14 16:33:32 +0300

    created map matrix & Tile class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arcade
 import math
 from itertools import product
+import random
 
 PATH = ''  # A:\ProjectGame\RmPG\\'
 
@@ -19,7 +20,10 @@ LEFT_FACING = DOWN_LEFT_FACING = 1
 UP_LEFT_FACING = UP_FACING = UP_RIGHT_FACING = 2
 DOWN_FACING = None
 
-MAP_SIZE = 1024 * 3 # *1000+ supported (*300 recommended maximum)
+MAP_SIZE = 1024 * 2 # *1000+ supported (*300 recommended maximum)
+TILE_SIZE = 800
+CORRIDOR_SIZE = 200
+MAX_TILES_IN_ROOM = 10
 
 
 def load_texture_pair(filename, x=0, y=0):
@@ -98,6 +102,19 @@ class Player(Entity):
         self.texture = self.walk_textures[self.cur_texture // 7][self.facing]
 
 
+class Tile:
+    def __init__(self, width=800, height=800):
+        self.width = width
+        self.height = height
+        self.exist = False
+        self.visited = False
+        self.room_id = 0
+        self.xr = width
+        self.xl = width
+        self.yu = height
+        self.yd = height
+
+
 class Game(arcade.Window):
 
     def __init__(self, width, height, name):
@@ -112,7 +129,9 @@ class Game(arcade.Window):
         self.background_texture = None
         self.background_sprites_matrix = None
 
-        self.map = None
+        self.map_matrix = None
+        self.tiles_count = None
+        self.room_list = None
 
         self.camera = None
 
@@ -133,13 +152,15 @@ class Game(arcade.Window):
 
         self.setup_background()
 
+        self.map_generator()
+
         self.scene.add_sprite_list("Walls", True)
         self.setup_border("Walls")
 
         self.scene.add_sprite_list("Player")
         self.person = Player()
-        self.person.center_x = SCREEN_WIDTH // 2
-        self.person.center_y = SCREEN_HEIGHT // 2
+        self.person.center_x = 200
+        self.person.center_y = 200
         self.person.center = self.person.center_x, self.person.center_y
 
         self.scene.add_sprite("Player", self.person)
@@ -190,6 +211,115 @@ class Game(arcade.Window):
                 wall.center_y = coordinate[1]
                 border_list.append(wall)
         self.scene.add_sprite_list(name, True, border_list)
+
+    def map_generator(self):
+        self.room_list = set()
+        self.tiles_count = MAP_SIZE // TILE_SIZE
+        min_size = TILE_SIZE//2 + CORRIDOR_SIZE//2
+        self.map_matrix = [[
+                Tile(width=random.randint(min_size, TILE_SIZE),
+                     height=random.randint(min_size, TILE_SIZE))
+                for _ in range(self.tiles_count)]
+                for _ in range(self.tiles_count)]
+
+        self.map_matrix[0][0].exist = True
+        self.map_matrix[0][0].visited = True
+        self.map_matrix[0][0].room_id = 1
+        self.room_list.add(self.map_matrix[0][0].room_id)
+        self.create_room(i=0, j=0)
+
+        for i in range(self.tiles_count):
+            for j in range(self.tiles_count):
+                if not self.map_matrix[i][j].visited:
+                    self.map_matrix[i][j].exist = random.getrandbits(1)
+                    self.map_matrix[i][j].visited = True
+                    if self.map_matrix[i][j].exist:
+                        self.create_room(i, j)
+
+    def create_room(self, i, j, current_count=1):
+        queue = [(i, j)]
+        pointer = 0
+
+        processing = True
+        while processing:
+            i = queue[pointer][0]
+            j = queue[pointer][1]
+
+            if current_count == MAX_TILES_IN_ROOM: return current_count
+            if i + 1 < self.tiles_count:
+                if not self.map_matrix[i+1][j].visited:
+                    self.map_matrix[i+1][j].visited = 1
+                    self.map_matrix[i+1][j].exist = random.getrandbits(1)
+                    if self.map_matrix[i+1][j].exist:
+                        self.map_matrix[i+1][j].room_id = self.map_matrix[i][j]
+                        current_count += 1
+                        queue.append((i + 1, j))
+                        self.neighbour_check(i+1, j)
+
+            if current_count == MAX_TILES_IN_ROOM: return current_count
+            if i - 1 >= 0:
+                if not self.map_matrix[i-1][j].visited:
+                    self.map_matrix[i-1][j].visited = 1
+                    self.map_matrix[i-1][j].exist = random.getrandbits(1)
+                    if self.map_matrix[i - 1][j].exist:
+                        self.map_matrix[i - 1][j].room_id = self.map_matrix[i][j]
+                        current_count += 1
+                        queue.append((i-1, j))
+                        self.neighbour_check(i-1, j)
+
+            if current_count == MAX_TILES_IN_ROOM: return current_count
+            if j + 1 < self.tiles_count:
+                if not self.map_matrix[i][j+1].visited:
+                    self.map_matrix[i][j+1].visited = 1
+                    self.map_matrix[i][j+1].exist = random.getrandbits(1)
+                    if self.map_matrix[i][j+1].exist:
+                        self.map_matrix[i][j+1].room_id = self.map_matrix[i][j]
+                        current_count += 1
+                        queue.append((i, j+1))
+                        self.neighbour_check(i, j+1)
+
+            if current_count == MAX_TILES_IN_ROOM: return current_count
+            if j - 1 >= 0:
+                if not self.map_matrix[i][j-1].visited:
+                    self.map_matrix[i][j-1].visited = 1
+                    self.map_matrix[i][j-1].exist = random.getrandbits(1)
+                    if self.map_matrix[i][j-1].exist:
+                        self.map_matrix[i][j-1].room_id = self.map_matrix[i][j]
+                        current_count += 1
+                        queue.append((i, j-1))
+                        self.neighbour_check(i, j-1)
+
+            if pointer == len(queue) - 1: processing = False
+            pointer += 1
+
+    def neighbour_check(self, i, j):
+        if i + 1 < self.tiles_count:
+            if self.map_matrix[i + 1][j].room_id == self.map_matrix[i][j].room_id:
+                self.map_matrix[i + 1][j].xl = 0
+                self.map_matrix[i][j].xr = 0
+                self.map_matrix[i + 1][j].yd = self.map_matrix[i][j].yd
+                self.map_matrix[i + 1][j].yu = self.map_matrix[i][j].yu
+
+        if i - 1 >= 0:
+            if self.map_matrix[i-1][j].room_id == self.map_matrix[i][j].room_id:
+                self.map_matrix[i-1][j].xr = 0
+                self.map_matrix[i][j].xl = 0
+                self.map_matrix[i-1][j].yd = self.map_matrix[i][j].yd
+                self.map_matrix[i-1][j].yu = self.map_matrix[i][j].yu
+
+        if j + 1 < self.tiles_count:
+            if not self.map_matrix[i][j + 1].room_id == self.map_matrix[i][j].room_id:
+                self.map_matrix[i][j+1].yd = 0
+                self.map_matrix[i][j].yu = 0
+                self.map_matrix[i][j+1].xr = self.map_matrix[i][j].xr
+                self.map_matrix[i][j+1].xl = self.map_matrix[i][j].xl
+
+        if j - 1 >= 0:
+            if not self.map_matrix[i][j - 1].room_id == self.map_matrix[i][j].room_id:
+                self.map_matrix[i][j-1].yu = 0
+                self.map_matrix[i][j].yd = 0
+                self.map_matrix[i][j-1].xr = self.map_matrix[i][j].xr
+                self.map_matrix[i][j-1].xl = self.map_matrix[i][j].xl
 
     def on_mouse(self):
 
@@ -245,23 +375,23 @@ class Game(arcade.Window):
         self.mouse_y = y
 
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.A:
+        if symbol == arcade.key.A or symbol == arcade.key.LEFT:
             self.person.correct_change_x += -PLAYER_MOVEMENT_SPEED  # x-axis
-        if symbol == arcade.key.D:
+        if symbol == arcade.key.D or symbol == arcade.key.RIGHT:
             self.person.correct_change_x += PLAYER_MOVEMENT_SPEED  # x-axis
-        if symbol == arcade.key.S:
+        if symbol == arcade.key.S or symbol == arcade.key.DOWN:
             self.person.correct_change_y += -PLAYER_MOVEMENT_SPEED  # y-axis
-        if symbol == arcade.key.W:
+        if symbol == arcade.key.W or symbol == arcade.key.UP:
             self.person.correct_change_y += PLAYER_MOVEMENT_SPEED  # y-axis
 
     def on_key_release(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.A:
+        if symbol == arcade.key.A or symbol == arcade.key.LEFT:
             self.person.correct_change_x += PLAYER_MOVEMENT_SPEED  # x-axis
-        if symbol == arcade.key.D:
+        if symbol == arcade.key.D or symbol == arcade.key.RIGHT:
             self.person.correct_change_x += -PLAYER_MOVEMENT_SPEED  # x-axis
-        if symbol == arcade.key.S:
+        if symbol == arcade.key.S or symbol == arcade.key.DOWN:
             self.person.correct_change_y += PLAYER_MOVEMENT_SPEED  # y-axis
-        if symbol == arcade.key.W:
+        if symbol == arcade.key.W or symbol == arcade.key.UP:
             self.person.correct_change_y += -PLAYER_MOVEMENT_SPEED  # y-axis
 
 
@@ -288,6 +418,31 @@ UI
 Menu
 setup_textures ???
 
+room generator
+есть сетка генерации комнат 800х800, в которой генерируются части комнат
+случайный x,y размеры в пределах 
+половина сетки + корридор (200) < x,y < сетка
+корридоры строго по середине квадрата сетки
+
+генерация двумя вложенными циклами 
+    генерация вглубь комнаты, 
+    вероятность следующей комнаты умножается на номер потомка или
+    или зависит от расстояния (в клетках) до комнаты-корня
+    динамическая вероятность (что-то вроде экспаненты) вероятность//кол-во присоединенных клеток
+    
+случайно вибирается одно из 2 состояний каждой стороны квадрата сетки
+- соседствует
+- не соседствует
+далее заполняется матрицей, которая проверяет есть ли связь с соседями, 
+если да, создает ещё часть комнаты
++ проверка на лимит соседей
+Если соседей не найдено происходят проверки
+- будет ли в этом квадрате комната
+    -  рандомит связи с соседними ЕЩЁ НЕ ПРОЙДЕННЫМИ комнатами
+
+
 '''
+
+
 
 #Если одновременно нажать на W и S - нельзя ходить вбок, а если A и D, то движение вверх-вниз работает как исправить
